50a8b090c4cf674e1a05920cd816ab1a7c354faf
Raiper34 <raipergm34@gmail.com>
2018-03-27 21:58:13 +0200

    reservation list

--- src/app/private/reservation/reservation.component.ts
+++ src/app/private/reservation/reservation.component.ts
@@ -4,6 +4,8 @@ import {AppState} from "../../shared/models/app.state";
 import {ReservationModel} from "../../shared/models/reservation.model";
 import {reservationListActions} from "../../shared/reducers/reservationList.reducer";
 import {Subscription} from "rxjs/Subscription";
+import {ApiService} from "../../shared/services/api.service";
+import {MzToastService} from "ng2-materialize";
 
 @Component({
   selector: 'app-reservation',
@@ -14,12 +16,40 @@ export class ReservationComponent implements OnDestroy {
 
   reservationListSubscription: Subscription;
   reservationListValue: ReservationModel[] = [];
+  reservationListSize = 0;
+  pages = [];
+  pageSize = 10;
+  currentPage = 0;
 
-  constructor(private store: Store<AppState>) {
+  constructor(private store: Store<AppState>,
+              private toastService: MzToastService,
+              private api: ApiService) {
     this.reservationListSubscription = store.pipe(select('reservationList')).subscribe((reservationList: ReservationModel[]) => {
       this.reservationListValue = reservationList;
     });
     this.store.dispatch({type: reservationListActions.GET_REQUEST});
+  }
+
+  get reservationList(): ReservationModel[] {
+    const reservationListValue = this.reservationListValue;
+    this.reservationListSize = this.reservationListValue.length;
+    this.pages = Array(Math.ceil(this.reservationListSize / this.pageSize), 10).fill(1).map((x, y) => x + y)
+    return reservationListValue
+      .filter((item, index) => index >= this.currentPage * this.pageSize && index < (this.currentPage * this.pageSize) + this.pageSize);
+  }
+
+  changePage(page: number): void {
+    this.currentPage = page;
+  }
+
+  deleteReservation(id: string): void {
+    this.api.delete('reservation', id).subscribe(
+      () => {
+        this.store.dispatch({type: reservationListActions.GET_REQUEST});
+        this.toastService.show('Delete successful!', 3000, 'green');
+        },
+      () => this.toastService.show('Delete error!', 3000, 'green')
+    );
   }
 
   ngOnDestroy(): void {
--- src/app/shared/models/reservation.model.ts
+++ src/app/shared/models/reservation.model.ts
@@ -1,7 +1,15 @@
+import {PersonModel} from "./person.model";
+import {RoomModel} from "./room.model";
+import {ServiceModel} from "./service.model";
+
 export interface ReservationModel {
-  id: number;
+  id?: number;
   dateFrom: number;
   dateTo: number;
-  payment: boolean;
+  paid: boolean;
   paymentType: number; //ENUM
+  creator?: PersonModel;
+  customer?: PersonModel;
+  reservedRoom?: RoomModel;
+  services?: ServiceModel[];
 }
